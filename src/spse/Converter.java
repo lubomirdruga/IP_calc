2276da89ed903ba736d792890a877cd3b858ca9f
Ľubomír Druga <drugalubomir@gmail.com>
2018-02-12 16:17:12 +0100

    `Converter.java` code cleanup

--- src/spse/Converter.java
+++ src/spse/Converter.java
@@ -9,31 +9,17 @@ public class Converter
     {
         String binaryOut = "";
 
-
         for (int i = 7; i >= 0; i--)
         {
             if(DECnumber >= Math.pow(2, i) || DECnumber == 1 && i == 0)
             {
                 binaryOut += "1";
-
                 DECnumber -= Math.pow(2, i);
             }
             else
-            {
                 binaryOut += "0";
-            }
-
-//            System.out.println("i:" + i);
-//            System.out.println("Dec Number: " + DECnumber);
-//            System.out.println("Bin Number: " + binaryOut);
         }
-
-
-
-//        System.out.println(DECnumber + " : " + binaryOut);
-
         return binaryOut;
-
     }
 
 
@@ -44,16 +30,11 @@ public class Converter
 
         for (int i = 0; i < binaryNumber.length(); i++)
         {
-
             if (binaryNumber.charAt(i) == '1')
-            {
                 DECNumber += Math.pow(2, exponent);
-            }
+
             exponent--;
         }
-
-       // System.out.println("Decimal from binary is:" + DECNumber);
-
         return DECNumber;
     }
 }
