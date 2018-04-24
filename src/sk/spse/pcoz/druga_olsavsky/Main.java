176d4e80b9ebe4e68888b43e6f129f8717192f5c
Ľubomír Druga <drugalubomir@gmail.com>
2018-04-24 11:32:31 +0200

    main method refactor, cleanup

--- src/sk/spse/pcoz/druga_olsavsky/Main.java
+++ src/sk/spse/pcoz/druga_olsavsky/Main.java
@@ -5,33 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Main extends Application {
 
-
-    //todo poriesit dedenie init metody z Drawer_Controller!
-
-    //TODO> prezriet vsetky metody, ktore mam z netu, a pridat k nim do komentov linky kvoli dokumentaicii!
-
-
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-//        Parent root = FXMLLoader.load(getClass().getResource("IPv4_parameters.fxml"));
-//        Parent root = FXMLLoader.load(getClass().getResource("Views/IPv6_parameters.fxml"));
-//        Parent root = FXMLLoader.load(getClass().getResource("Converter.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("Views/IPv4_info.fxml"));
-
+        Parent root = FXMLLoader.load(getClass().getResource("Views/IPv4_parameters.fxml"));
 
         primaryStage.setTitle("IP subnetting");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
-
 
     public static void main(String[] args)
     {
